82c8ab5404fa32ee4540e884f323bb8685b8686c
Nilesh Kotwal <100336356+Nilesh4141@users.noreply.github.com>
2022-12-12 06:34:22 +0530

    Update food.js

--- food.js
+++ food.js
@@ -3,7 +3,7 @@ const mealList = document.getElementById('meal');
 const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
 
-// event listeners
+
 searchBtn.addEventListener('click', getMealList);
 mealList.addEventListener('click', getMealRecipe);
 recipeCloseBtn.addEventListener('click', () => {
